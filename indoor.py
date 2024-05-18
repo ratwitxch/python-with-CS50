29291a431f5fe84e065afe377fcfdf3184b0c976
˚ ༘♡ , bee <135217941+ratwitxch@users.noreply.github.com>
2024-05-18 20:10:53 +0700

    5 | whisper & 0.75x
    Continuing my CS50 Python journey after working on a lot of other things. It wasn't much, but I found some problems to mess around with. Solved 2 and I got to relearn commands and functions I've forgotten these few months.

---
+++ indoor.py
@@ -0,0 +1,8 @@
+input("welcome to the whispering willows!\n")
+input("where all loud noises are turned to whispers in the wind.\n")
+
+msg = input("don't believe me? try it out..\n=> ")
+msg = msg.lower()
+
+print("\n=> " + msg)
+print(" ")
---
+++ playback.py
@@ -0,0 +1,6 @@
+input("\nu talk so fast..\n")
+input("I'm sry, it's not u. It's me.\n")
+msg = input("pls write ur message here so i can understand it better.\n=> ")
+
+msg = msg.replace(" ", "...")
+print("\n" + msg + "\n")
